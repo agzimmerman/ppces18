be554286e080882c8614e62ca64b7e05a5780eac
Alexander G. Zimmerman <alexander.zimmerman@rwth-aachen.de>
2018-03-12 17:29:59 +0100

    pingPong runs, but result looks wrong

--- 1_pingPong/a/pingPong.c
+++ 1_pingPong/a/pingPong.c
@@ -1,40 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <assert.h>
 
 int main(int argc, char **argv)
 {
+    int ierr;
     int myRank, numProcs;
     int pingCount = 42;
     int pongCount = 0;
+    int pinger = 0;
+    int ponger = 1;
+    int count = 1;
+    int tag = 0;
 
-    // TODO: Initialize MPI
+    // Initialize MPI
+    ierr = MPI_Init(&argc, &argv);
 
-    // TODO: Find out MPI communicator size and process rank
 
-    // TODO: Have only the first process execute the following code
-    if (/* TODO */)
+    // Find out MPI communicator size and process rank
+    ierr = MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    
+    assert(numProcs == 2);  // This is two-player ping-pong!
+    
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    
+
+    // Have only the first process execute the following code
+    if (myRank == pinger)
     {
+        // Send pingCount
         printf("Sending Ping (# %i)\n", pingCount);
-        // TODO: Send pingCount
 
-        // TODO: Receive pongCount
+        MPI_Send(&pingCount, count, MPI_INT, ponger, tag, MPI_COMM_WORLD);
+
+
+        // Receive pongCount
+        MPI_Recv(&pongCount, count, MPI_INT, ponger, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         printf("Received Pong (# %i)\n", pongCount);
+
+        pingCount = pongCount;
     }
-    // TODO: Do proper receive and send in any other process
+    // Do proper receive and send in any other process
     else
     {
-        // TODO: Receive pingCount
+        // Receive pingCount
+        MPI_Recv(&pingCount, count, MPI_INT, pinger, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         printf("Received Ping (# %i)\n", pingCount);
 
-        // TODO: calculate and send pongCount
+        // calculate and send pongCount
+        pongCount *= -pingCount;
+        
         printf("Sending Pong (# %i)\n", pongCount);
+
+        MPI_Send(&pongCount, count, MPI_INT, pinger, tag, MPI_COMM_WORLD);
 
     }
 
-    // TODO: Finalize MPI
+    // Finalize MPI
+    MPI_Finalize();
 
     return 0;
 }
